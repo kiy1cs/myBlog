69bd37780fa7b30f3981fcb8be63217b4e6ebc47
kiy1cs <kiyukiyu1234@gmail.com>
2026-03-23 23:37:24 +0800

    initial config

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
+	title: "kiy1cs's blog",
+	subtitle: "home",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/cool night sky.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
@@ -46,33 +46,28 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/kiy1cs/myBlog", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/pfp.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "kiy1cs",
+	bio: "a taiwanese student larpmaxxing",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+  		{
+			name: "Instagram",
+			icon: "fa6-brands:instagram", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://www.instagram.com/kiy1cs/",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/kiy1cs",
 		},
 	],
 };
---
+++ src/content/posts/About Me.md
@@ -0,0 +1,42 @@
+---
+title: About Me
+published: 2026-03-23
+description: 'About kiy1cs'
+image: ''
+tags: []
+category: 'About'
+draft: false 
+lang: 'en'
+---
+## main
+- a high school student 🧑‍🎓
+- from taiwan and loves his country 🇹🇼
+- plays games, aside from software i also like military and history 🎮
+## contact me
+- discord: @realkiy1cs325
+- matrix: https://matrix.to/#/@realkiy1cs325:matrix.org
+- github: https://github.com/kiy1cs
+- instagram: https://www.instagram.com/kiy1cs/
+## tech
+### wants to learn
+- game development 🎮
+- cyber security 🛡️
+- building a homelab 🖥️
+- more programming languages ⌨️
+### PC specs
+- CPU: Ryzen 3 3100
+- GPU: RX 570
+- A520M AORUS ELITE, 16GB RAM DDR4
+- dual boots windows and arch linux btwbtwbtw
+## games
+### favorite
+- roblox: https://www.roblox.com/users/452674632/profile
+- note: im not brainrot kids
+### currently plays
+- terraria
+- war thunder
+- euro truck
+### wants to play
+- ready or not
+- hearts of iron 4
+- cyberpunk
